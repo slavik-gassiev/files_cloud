c9c83e93077b8095851f0be5841aed02f805410e
slavik-gassiev <slavikgassiev1872002@gmail.com>
2025-01-05 17:53:35 +0300

    corrected flyway version

---
+++ src/main/java/com/slava/FilesCloudApplication.java
@@ -0,0 +1,11 @@
+package com.slava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesCloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FilesCloudApplication.class, args);
+    }
+}
