2c654baddd8d96a41404bc06c25f23c6a7215e80
slavik-gassiev <slavikgassiev1872002@gmail.com>
2025-01-06 15:42:32 +0300

    Added UserService

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,49 @@
+package service;
+
+import com.slava.entity.User;
+import com.slava.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return Optional.ofNullable(userRepository.findByUsername(username));
+    }
+
+    public User registerUser(User user) {
+        // Проверка существования пользователя
+        findByUsername(user.getUsername()).ifPresent(u -> {
+            throw new IllegalArgumentException("User already exists");
+        });
+
+        // Хеширование пароля и сохранение пользователя
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
+
