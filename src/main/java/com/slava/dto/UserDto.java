b02ca7f9850865a8eafad222473f767efc3884a4
slavik-gassiev <slavikgassiev1872002@gmail.com>
2025-02-21 15:25:31 +0300

    corrected username and folder name in register.html and list.html

--- src/main/java/com/slava/dto/UserDto.java
+++ src/main/java/com/slava/dto/UserDto.java
@@ -2,6 +2,7 @@ package com.slava.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,7 @@ public class UserDto implements Serializable {
 
     @NotBlank(message = "Username cannot be empty")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "Username can only contain lowercase Latin letters and numbers.")
     private String username;
 
     @NotBlank(message = "Password cannot be empty")
